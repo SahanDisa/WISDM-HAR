1a42191c9671a62b7c843c16dc505e898733786d
Sahan Dissanayaka <t.sahan998@gmail.com>
2024-12-03 17:55:54 -0500

    [ADD] CNN model after tuning

--- src/models/CNN.py
+++ src/models/CNN.py
@@ -18,15 +18,15 @@ from tqdm import tqdm
 class CNN(nn.Module):
     def __init__(self, input_size, num_classes):
         super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(3, 3), padding=(1, 1))
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=(3, 3), padding=(1, 1))
         self.relu1 = nn.ReLU()
         self.pool1 = nn.MaxPool2d(kernel_size=(2, 1))
         
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), padding=(1, 1))
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=128, kernel_size=(3, 3), padding=(1, 1))
         self.relu2 = nn.ReLU()
         self.pool2 = nn.MaxPool2d(kernel_size=(2, 1))
         
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), padding=(1, 1))
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=256, kernel_size=(3, 3), padding=(1, 1))
         self.relu3 = nn.ReLU()
         self.pool3 = nn.MaxPool2d(kernel_size=(2, 1))
         
@@ -34,24 +34,17 @@ class CNN(nn.Module):
         self.flatten_dim = 128 * (100 // (2 * 2 * 2)) * (3 // 1)
         
         # Define the fully connected layers
-        self.fc1 = nn.Linear(self.flatten_dim, 128)
+        self.fc1 = nn.Linear(self.flatten_dim, 256)
         self.relu_fc = nn.ReLU()
-        self.fc2 = nn.Linear(128, num_classes)
+        self.fc2 = nn.Linear(256, num_classes)
 
     def forward(self, x):
-        # Reshape input to (batch_size, 1, 100, 3)
-        x = x.unsqueeze(1)
-        
-        # Conv2D layers
+        x = x.unsqueeze(1)  # Add channel dimension
         x = self.pool1(self.relu1(self.conv1(x)))
         x = self.pool2(self.relu2(self.conv2(x)))
         x = self.pool3(self.relu3(self.conv3(x)))
-        
-        # Flatten
-        x = x.view(x.size(0), -1)
-        
-        # Fully connected layers
-        x = self.relu_fc(self.fc1(x))
+        x = x.view(x.size(0), -1)  # Flatten
+        x = self.dropout(self.relu_fc(self.fc1(x)))
         x = self.fc2(x)
         
         return x
